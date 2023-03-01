e22b11f6c69895d3e99ba854359472c866b3257d
cw <fromnowwon@gmail.com>
2023-03-01 18:08:48 +0900

    fix: 'use client' 지시어 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/styles/theme';
---
+++ app/sup/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { Button } from '@mui/material';
+
+const Sup: React.FC = () => <Button variant="contained">button</Button>;
+
+export default Sup;
